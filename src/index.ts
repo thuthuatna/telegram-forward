db659dbb46e048c3cc59288cf5306ce56894d6db
anhnn <thuthuatna@gmail.com>
2021-08-22 17:05:19 +0700

    Setup package environment

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { app } from './app';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export function hello(world: string = 'world'): void {
   return console.log(`Hello ${world}! `);
 }
 
 const start = (async function () {
-  app.listen(3000, () => {
+  app.listen(process.env.PORT, () => {
     console.log('Listening on port 3000!');
   });
 })();
